86fd7df3825926b1cd1ea9d5061db1ffe4e60a46
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-08 13:55:16 +0200

    Add IsOwnerOrReadOnly permission & api-auth url

--- app_profile/views.py
+++ app_profile/views.py
@@ -1,21 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
+from organisation_app.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
+
+    queryset = Profile.objects.all().order_by("-created_at")
+    serializer_class = ProfileSerializer
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner.
     """
+
+    permission_classes = [IsOwnerOrReadOnly]
 
     queryset = Profile.objects.all().order_by("-created_at")
     serializer_class = ProfileSerializer
