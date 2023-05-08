c4bc71b77a32367783a31a195915e86c350fb1ca
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-08 13:39:00 +0200

    Add profile detail view and url

--- app_profile/urls.py
+++ app_profile/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("profiles/", views.ProfileList.as_view()),
+    path("profile/<int:pk>/", views.ProfileDetail.as_view()),
 ]
--- app_profile/views.py
+++ app_profile/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,3 +10,12 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+
+    queryset = Profile.objects.all().order_by("-created_at")
+    serializer_class = ProfileSerializer
