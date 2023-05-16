5a86b838dbdb78903a750f8eaeb551daefc3135f
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-16 15:28:04 +0200

    Add counts to profile serializers &views

--- app_profile/serializers.py
+++ app_profile/serializers.py
@@ -1,21 +1,43 @@
 from rest_framework import serializers
 from .models import Profile
 from app_calendar.serializers import CalendarSerializer
+from app_follower.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
     calendar = CalendarSerializer(read_only=True, source="owner.calendar")
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
         fields = [
             "id",
             "owner",
+            "calendar",
             "created_at",
             "updated_at",
             "name",
             "content",
             "image",
-            "calendar",
+            "is_owner",
+            "following_id",
+            "posts_count",
+            "followers_count",
+            "following_count",
         ]
