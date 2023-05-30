9289f793e8352b541daa44b3ae0e279c80af8862
dragon-fire-fly <ek93@hotmail.co.uk>
2023-05-30 13:35:30 +0200

    Add user calendar serializer w/ serialized events

--- app_calendar/serializers.py
+++ app_calendar/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from .models import Calendar
+from app_event.models import Event
+from app_event.serializers import EventSerializer
 
 
 class CalendarSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Calendar
+        fields = ["id", "owner", "events", "timezone"]
+
+
+class UserCalendarSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    events = EventSerializer(many=True)
 
     class Meta:
         model = Calendar
