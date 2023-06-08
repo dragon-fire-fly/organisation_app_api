e68a31e8169826506451bdad494186fad4f623a5
dragon-fire-fly <ek93@hotmail.co.uk>
2023-06-08 12:05:05 +0200

    Update ci database to sqlite3

--- organisation_app/settings.py
+++ organisation_app/settings.py
@@ -131,13 +131,9 @@ if os.getenv("GITHUB_WORKFLOW"):
     # check if in GITHUB ACTION MODE
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "github-actions",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "localhost",
-            "PORT": "5432",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+            }
     }
 else:
     # check if unittest running locally
